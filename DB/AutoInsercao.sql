27587a8fa745720ee04d21ac535c04e06530475c
MaiconDewes <dewesmaicon@gmail.com>
2017-08-30 21:26:32 -0300

    Sql do banco e da auto inserção
    A auto inserção deve ser utilizada somente com um banco de dados novo.

---
+++ DB/AutoInsercao.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO TipoUsuario (descricao)
+VALUES ('coordenador');
+
+INSERT INTO TipoUsuario (descricao)
+VALUES ('professor');
+
+INSERT INTO TipoUsuario (descricao)
+VALUES ('aluno');
+
+
+INSERT INTO CategoriaNoticia (descricao, cor)
+VALUES ('Carreira', '#FFFF00');
+
+INSERT INTO CategoriaNoticia (descricao, cor)
+VALUES ('Tecnologia', '#FF0000');
+
+INSERT INTO CategoriaNoticia (descricao, cor)
+VALUES ('Hardware', '#8B4513');
+
+INSERT INTO CategoriaNoticia (descricao, cor)
+VALUES ('Software', '#0000FF');
+
+INSERT INTO CategoriaNoticia (descricao, cor)
+VALUES ('Tendência', '#00FF00');
