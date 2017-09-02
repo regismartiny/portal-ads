3577f7304f0d73592eaa6a61091a55eef0052d06
jacksonvalereal <jacksonvalereal@gmail.com>
2017-09-02 09:29:34 -0300

    3 usuários

--- db/AutoInsercao.sql
+++ db/AutoInsercao.sql
@@ -23,3 +23,7 @@ VALUES ('Software', '#0000FF');
 
 INSERT INTO CategoriaNoticia (descricao, cor)
 VALUES ('Tendência', '#00FF00');
+
+INSERT INTO Usuario VALUES (1,'7777777','testeCoordenador','emailtesteportalads@gmail.com','123456789',1,3);
+INSERT INTO Usuario VALUES (2,'8888888','testeProfessor','emailtesteportalads@gmail.com','123456789',1,2);
+INSERT INTO Usuario VALUES (3,'121212121212','testeAluno','emailtesteportalads@gmail.com','123456789',1,1);
