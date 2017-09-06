fb7586f013b5dd0154455ef783e6e7b03ce8bf4e
babalubas090 <rick@interword.com.br>
2017-09-06 17:06:17 -0600

    Update do Controle do Usuario

--- controle/ControleUsuario.class.php
+++ controle/ControleUsuario.class.php
@@ -1,19 +1,20 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT']."/portal-ads-master/modelo/Usuario.class.php";
+include $_SERVER['DOCUMENT_ROOT']."/portal-ads/modelo/Usuario.class.php";
 
 class ControleUsuario{
-	
-	
-	
 	public function verificaLogin($dados) {
 		$usuario = new Usuario(null,$dados['matricula'],null,null,$dados['senha'],null,null);
 		$cadastrado = $usuario->isCadastrado();
 		return $cadastrado;
 	}
 	
-	
-	
-	
+	public function inserir($dados){
+		
+		$usuario = new Usuario(null,$dados['matricula'],$dados['nome'],$dados['email'],$dados['senha'],1,$dados['tipoUsuario_id']);
+		$usuario->inserir();
+		header("location:../visao/lstContato.php");
+	}	
+		
 }
 
 ?>
