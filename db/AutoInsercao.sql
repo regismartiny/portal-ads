165b22976f2f9d18fca5d5532644a48c4a947fd7
jacksonvalereal <jacksonvalereal@gmail.com>
2017-09-06 21:53:24 -0300

    Update AutoInsercao.sql

--- db/AutoInsercao.sql
+++ db/AutoInsercao.sql
@@ -1,12 +1,9 @@
 
-INSERT INTO TipoUsuario (descricao)
-VALUES ('coordenador');
+INSERT INTO TipoUsuario VALUES (1,'coordenador');
 
-INSERT INTO TipoUsuario (descricao)
-VALUES ('professor');
+INSERT INTO TipoUsuario VALUES (2,'professor');
 
-INSERT INTO TipoUsuario (descricao)
-VALUES ('aluno');
+INSERT INTO TipoUsuario VALUES (3,'aluno');
 
 
 INSERT INTO CategoriaNoticia (descricao, cor)
@@ -24,6 +21,7 @@ VALUES ('Software', '#0000FF');
 INSERT INTO CategoriaNoticia (descricao, cor)
 VALUES ('Tendência', '#00FF00');
 
-INSERT INTO Usuario VALUES (1,'7777777','testeCoordenador','emailtesteportalads@gmail.com','123456789',1,3);
-INSERT INTO Usuario VALUES (2,'8888888','testeProfessor','emailtesteportalads@gmail.com','123456789',1,2);
-INSERT INTO Usuario VALUES (3,'121212121212','testeAluno','emailtesteportalads@gmail.com','123456789',1,1);
+INSERT INTO Usuario VALUES (1,'admin','testeCoordenador','emailtesteportalads@gmail.com','admin',1,1);
+INSERT INTO Usuario VALUES (2,'7777777','testeProfessor','emailtesteportalads@gmail.com','123456789',1,2);
+INSERT INTO Usuario VALUES (3,'121212121212','testeAluno','emailtesteportalads@gmail.com','123456789',1,3);
+
