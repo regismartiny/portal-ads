52a2d6f84acc350730d30835cf43eee0fe71aecd
babalubas090 <rick@interword.com.br>
2017-09-06 19:15:33 -0600

    Criado o modelo de tipousuario
    Criado o modelo de tipousuario

---
+++ modelo/TipoUsuario.class.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT']."/portal-ads/db/MySQL.class.php";
+	class TipoUsuario{
+		private $id;
+		private $descricao;
+		
+		public function __construct($id = null, $descricao = null){
+			$this->id = $id;
+			$this->descricao = $descricao;	
+		}
+		
+		public function getId(){
+			return $this->id;
+		}
+		
+		public function setId($id){
+			$this->id = $id;
+		}
+		
+		public function getDescricao(){
+			return $this->descricao;
+		}
+		
+		public function setDescricao($descricao){
+			$this->descricao = $descricao;
+		}
+		
+		public function getTipoUsuario(){
+			$con = new MySQL();
+			$sql = "SELECT * FROM tipousuario";
+			$resultados = $con->consulta($sql);
+			return $resultados;
+		}
+	}
+?>
