c3b302513b6e7cf70f5836b15c9f3f7925786867
regismartiny <regismartiny@gmail.com>
2017-09-08 11:22:48 -0300

    Ajuste variaveis de ambiente banco

--- db/Configuracao.inc.php
+++ db/Configuracao.inc.php
@@ -1,6 +1,10 @@
 <?php
-define("HOST","localhost");
-define("USUARIO","root");
-define("SENHA","");
-define("BANCO","portal-ads");
+$db_host = getenv('MYSQL_HOST') !== false ? getenv('MYSQL_HOST') : 'localhost';
+$db_user = getenv('MYSQL_USER') !== false ? getenv('MYSQL_USER') : 'root';
+$db_pass = getenv('MYSQL_PASSWORD') !== false ? getenv('MYSQL_PASSWORD') : '';
+$db_database = getenv('MYSQL_DATABASE') !== false ? getenv('MYSQL_DATABASE') : 'portal-ads';
+define("HOST", $db_host);
+define("USUARIO", $db_user);
+define("SENHA", $db_pass);
+define("BANCO", $db_database);
 ?>
