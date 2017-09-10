9b853b406e0516799fc6ea5934346b3b85897668
babalubas090 <rick@interword.com.br>
2017-09-09 22:15:38 -0300

    Updates

--- db/AutoInsercao.sql
+++ db/AutoInsercao.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO TipoUsuario VALUES (1,'Coordenador');
 
 INSERT INTO TipoUsuario VALUES (2,'Professor');
@@ -21,7 +20,9 @@ VALUES ('Software', '#0000FF');
 INSERT INTO CategoriaNoticia (descricao, cor)
 VALUES ('Tendência', '#00FF00');
 
-INSERT INTO Usuario VALUES (1,'admin','testeCoordenador','emailtesteportalads@gmail.com','admin',1,1);
-INSERT INTO Usuario VALUES (2,'7777777','testeProfessor','emailtesteportalads@gmail.com','123456789',1,2);
-INSERT INTO Usuario VALUES (3,'121212121212','testeAluno','emailtesteportalads@gmail.com','123456789',1,3);
 
+INSERT INTO Usuario VALUES (1,'admin','testeCoordenador','emailtesteportalads@gmail.com','admin',1,1);
+
+INSERT INTO Usuario VALUES (2,'7777777','testeProfessor','emailtesteportalads@gmail.com','123456789',1,2);
+
+INSERT INTO Usuario VALUES (3,'121212121212','testeAluno','emailtesteportalads@gmail.com','123456789',1,3);
