f6eddb086012f43cabd8bdeb2ad0ca7fce499809
Régis Martiny <regismartiny@gmail.com>
2017-09-10 18:12:12 -0300

    Listagem de categoria de noticias

---
+++ modelo/CategoriaNoticia.class.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT']."/db/MySQL.class.php";
+
+class CategoriaNoticia
+{
+    private $id;
+    private $descricao;
+    private $cor;
+        
+    public function __construct($id = null, $descricao = null, $cor = null)
+    {
+        $this->id = $id;
+        $this->descricao = $descricao;
+        $this->cor = $cor;
+    }
+        
+    public function getId()
+    {
+        return $this->id;
+    }
+        
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+        
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+        
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function getCor()
+    {
+        return $this->cor;
+    }
+        
+    public function setCor($cor)
+    {
+        $this->cor = $cor;
+    }
+
+    public function listarTodos()
+    {
+        $con = new MySQL();
+        $sql = "SELECT * FROM CategoriaNoticia";
+        $resultados = $con->consulta($sql);
+        if (!empty($resultados)) {
+            $categorias = array();
+            foreach ($resultados as $resultado) {
+                $categoria = new CategoriaNoticia();
+                $categoria->setId($resultado["id"]);
+                $categoria->setDescricao($resultado["descricao"]);
+				$categoria->setCor($resultado["cor"]);
+				$categorias[] = $categoria;
+            }
+            return $categorias;
+        } else {
+            return false;
+        }
+    }
+}
