c10f64638524cf44ced23efaf15ff04a97cfd1fa
Regis Martiny <regis.martiny@cwi.com.br>
2017-09-11 11:24:48 -0300

    Topo sem margens

--- visao/index.php
+++ visao/index.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <title>Portal ADS</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
-    <div id="topo" class="container-fluid fixed-top">
+    <div id="topo" class="fixed-top">
         <div class="row">
             <div class="col-auto mr-auto">
                 <img src="img/logo.jpg" id="logo">
