8f64a264759e6d89db37588f0666772b301f731b
Regis Martiny <regis.martiny@cwi.com.br>
2017-09-12 13:45:22 -0300

    Ajustes layout index e contato

--- visao/paginas-publicas/contato.php
+++ visao/paginas-publicas/contato.php
@@ -1,41 +1,41 @@
-<div class="row justify-content-center">
-    <div class="col-5">
-        <form method='post' action='contato.php'>
-            <div class="form-group row">
-                <label for="categoria" class="col-sm-4 col-md-4 col-form-label">Tipo de mensagem:</label>
-                <div class="col-sm-10 col-md-8">
-                    <select class="form-control" id="categoria" name="categoria" required>
-                        <?php 
-                            include_once $_SERVER['DOCUMENT_ROOT']."/modelo/CategoriaNoticia.class.php";
+<div class="row h-100">
+            <div class="col-sm-12 col-md-8 col-lg-6 mx-auto my-auto">
+                <form method='post' action='contato.php'>
+                    <div class="form-group row">
+                        <label for="categoria" class="col-sm-12 col-md-4 col-form-label">Tipo de mensagem:</label>
+                        <div class="col-sm-12 col-md-8">
+                        <select class="form-control" id="categoria" name="categoria" required>
+                            <?php 
+                                include_once $_SERVER['DOCUMENT_ROOT']."/modelo/CategoriaNoticia.class.php";
 
-                            $categoria = new CategoriaNoticia();
-                            $categorias = $categoria->listarTodos();
-                            foreach ($categorias as $categoria) {
-                                echo '<option value="' . $categoria->getId() . '">' . $categoria->getDescricao() . '</option>';
-                            }
-                        ?>
-                    </select>
-                </div>
+                                $categoria = new CategoriaNoticia();
+                                $categorias = $categoria->listarTodos();
+                                foreach ($categorias as $categoria) {
+                                    echo '<option value="' . $categoria->getId() . '">' . $categoria->getDescricao() . '</option>';
+                                }
+                            ?>
+                        </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="nome" class="col-sm-12 col-md-4 col-form-label">Seu nome:</label>
+                        <div class="col-sm-12 col-md-8">
+                            <input type="text" class="form-control" id="nome" name='nome' required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="email" class="col-sm-12 col-md-4 col-form-label">Seu e-mail:</label>
+                        <div class="col-sm-12 col-md-8">
+                            <input type="email" class="form-control" id="email" name='email' required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="mensagem" class="col-sm-12 col-md-4 col-form-label">Digite sua mensagem:</label>
+                        <div class="col-sm-12 col-md-8">
+                            <textarea class="form-control" rows="4" id="mensagem" name="mensagem" required></textarea>
+                        </div>
+                    </div>
+                    <input type='submit' class='btn btn-primary btn-lg btn-block' name='enviar' value='Enviar'>
+                </form>
             </div>
-            <div class="form-group row">
-                <label for="nome" class="col-sm-4 col-md-4 col-form-label">Seu nome:</label>
-                <div class="col-sm-10 col-md-8">
-                    <input type="text" class="form-control" id="nome" name='nome' required>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="email" class="col-sm-4 col-md-4 col-form-label">Seu e-mail:</label>
-                <div class="col-sm-10 col-md-8">
-                    <input type="email" class="form-control" id="email" name='email' required>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="mensagem" class="col-sm-4 col-md-4 col-form-label">Digite sua mensagem:</label>
-                <div class="col-sm-10 col-md-8">
-                    <textarea class="form-control" rows="4" id="mensagem" name="mensagem" required></textarea>
-                </div>
-            </div>
-            <input type='submit' class='btn btn-primary btn-lg btn-block' name='enviar' value='Enviar'>
-        </form>
-    </div>
-</div>
+        </div>
