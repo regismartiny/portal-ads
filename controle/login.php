7c1658a0d968a96a3f369d22c9a978f34607e0ce
jacksonvalereal <jacksonvalereal@gmail.com>
2017-09-15 20:58:39 -0300

    condição para iniciar a sessão

--- controle/login.php
+++ controle/login.php
@@ -18,17 +18,19 @@ if (!empty($_POST) && isset($_POST['matricula']) && isset($_POST['senha'])
     $resultado = $cadastrado;
 
     if ($cadastrado) {
-        session_start();
         
-        $usuario = $cUsuario->listarUm($_POST['matricula']);
-                        
-        $_SESSION["nomeUsuario"] = $usuario->getNome();
-        $_SESSION['matricula'] = $usuario->getSiapeMatricula();
-        $_SESSION['email'] = $usuario->getEmail();
-        $_SESSION['tipoUsuario'] = $usuario->getTipoUsuario_id();
+        
+        if(!isset($_SESSION)){
+            session_start();
+            $usuario = $cUsuario->listarUm($_POST['matricula']);
+            $_SESSION["nomeUsuario"] = $usuario->getNome();
+            $_SESSION['matricula'] = $usuario->getSiapeMatricula();
+            $_SESSION['email'] = $usuario->getEmail();
+            $_SESSION['tipoUsuario'] = $usuario->getTipoUsuario_id();
 
-        $tipoUsuario = array('tipoUsuario' => $_SESSION['tipoUsuario']);
-        $resultado = json_encode($tipoUsuario, JSON_FORCE_OBJECT);
+            $tipoUsuario = array('tipoUsuario' => $_SESSION['tipoUsuario']);
+            $resultado = json_encode($tipoUsuario, JSON_FORCE_OBJECT);
+        }
     }
     echo json_encode($resultado);
 }
