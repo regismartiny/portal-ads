28d1225fa7ba5123cc8de09dcc04a60fd5eb928a
Régis Martiny <regismartiny@gmail.com>
2017-09-15 00:28:59 -0300

    Ajuste direcionamento links

--- visao/index.js
+++ visao/index.js
@@ -1,25 +1,29 @@
-$(document).ready(function () {
+$(document).ready(function() {
     //////////////////////////////Controle de navegação de páginas//////////////////////////
     var newHash = '',
         $conteudoCentral = $('#container-conteudo');
 
-    $('body').delegate('a', 'click', function () {
-        window.location.hash = $(this).attr('href');
-        return false;
+    $('body').delegate('a', 'click', function() {
+        let attr = $(this).attr('href')
+        if (attr.startsWith('/')) {
+            window.location.hash = attr;
+            return false;
+        }
+        return true;
     });
 
     // Not all browsers support hashchange
     // For older browser support: http://benalman.com/projects/jquery-hashchange-plugin/
-    $(window).on('hashchange', function () {
+    $(window).on('hashchange', function() {
         newHash = window.location.hash.substr(1);
-        $conteudoCentral.load('/visao' + newHash, function () {
+        $conteudoCentral.load('/visao' + newHash, function() {
             console.log('Página carregada: ' + newHash);
         });
     });
     ///////////////////////////////////////////////////////////////////////////////////
 
     /////////////////////////////Controle do menu/////////////////////////////////////////
-    $('.nav-item').click(function () {
+    $('.nav-item').click(function() {
         let menuExpandido = $('#navbar-toggler').attr('aria-expanded') === 'true';
         if (menuExpandido) {
             //fecha menu ao clicar
