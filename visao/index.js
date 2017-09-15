b1306f92a06504d29a54129a3930691bee44227e
Regis Martiny <regis.martiny@cwi.com.br>
2017-09-15 13:21:38 -0300

    Ajuste comportamento menu

--- visao/index.js
+++ visao/index.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //////////////////////////////Controle de navegação de páginas//////////////////////////
     var newHash = '',
         $conteudoCentral = $('#container-conteudo');
 
-    $('body').delegate('a', 'click', function() {
+    $('body').delegate('a', 'click', function () {
         let attr = $(this).attr('href')
         if (attr.startsWith('/')) {
             window.location.hash = attr;
@@ -14,22 +14,36 @@ $(document).ready(function() {
 
     // Not all browsers support hashchange
     // For older browser support: http://benalman.com/projects/jquery-hashchange-plugin/
-    $(window).on('hashchange', function() {
+    $(window).on('hashchange', function () {
         newHash = window.location.hash.substr(1);
-        $conteudoCentral.load('/visao' + newHash, function() {
+        $conteudoCentral.load('/visao' + newHash, function () {
             console.log('Página carregada: ' + newHash);
         });
     });
     ///////////////////////////////////////////////////////////////////////////////////
 
     /////////////////////////////Controle do menu/////////////////////////////////////////
-    $('.nav-item').click(function() {
-        let menuExpandido = $('#navbar-toggler').attr('aria-expanded') === 'true';
-        if (menuExpandido) {
-            //fecha menu ao clicar
-            $('.collapse').collapse('toggle');
+    $('.dropdown-item').click(function () {
+        if (isMenuExpandido()) {
+            toggleMenu();
+        }
+        $('.dropdown-item').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.nav-item').click(function () {
+        let isDropDown = $(this).hasClass('dropdown');
+        if (isMenuExpandido() && !isDropDown) {
+            toggleMenu();
         }
         $('.nav-item').removeClass('active');
         $(this).addClass('active');
     });
 });
+
+function isMenuExpandido() {
+    $('#navbar-toggler').attr('aria-expanded') === 'true';
+}
+
+function toggleMenu() {
+    $('.collapse').collapse('toggle');
+}
