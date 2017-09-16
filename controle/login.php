37a2d7da0bac3bbe09e7b578fe8017da4d471ee8
Jackson <jacksonvalereal@gmail.com>
2017-09-16 17:31:04 -0300

    c

--- controle/login.php
+++ controle/login.php
@@ -27,7 +27,7 @@ if (!empty($_POST) && isset($_POST['matricula']) && isset($_POST['senha'])
 			
 				session_start();
 				$usuario = $cUsuario->listarUm($_POST['matricula']);
-				$_SESSION["nomeUsuario"] = $usuario->getNome();
+				$_SESSION['nomeUsuario'] = $usuario->getNome();
 				$_SESSION['matricula'] = $usuario->getSiapeMatricula();
 				$_SESSION['email'] = $usuario->getEmail();
 				$_SESSION['tipoUsuario'] = $usuario->getTipoUsuario_id();
