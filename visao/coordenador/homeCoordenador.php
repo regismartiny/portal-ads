e0b1633d9ab4e8cc51a971050530ef3e1aff598d
Jackson <jacksonvalereal@gmail.com>
2017-09-16 17:30:04 -0300

    c

--- visao/coordenador/homeCoordenador.php
+++ visao/coordenador/homeCoordenador.php
@@ -22,13 +22,14 @@
                 <form method="post" action="/controle/logout.php">
                     <div class="form-group row">
                         <h1 class="col-sm-12 col-form-label">Home do Coordenador</h1>
-						<?php echo $_SESSION["nomeUsuario"];?>
+						
                     </div>
                     <input type="submit" class="btn btn-primary btn-lg btn-block" name="botao" value="LogOut">
                 </form>
             </div>
         </div>
     </div>
+	<?php echo $_SESSION['nomeUsuario'];?>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
