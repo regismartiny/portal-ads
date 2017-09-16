dbec9654928cca6ac6e24470e7b748981e3c4ad0
jacksonvalereal <jacksonvalereal@gmail.com>
2017-09-15 21:57:13 -0300

    add metodo verificaSenha para separar os erros

--- controle/ControleUsuario.class.php
+++ controle/ControleUsuario.class.php
@@ -3,11 +3,18 @@ include $_SERVER['DOCUMENT_ROOT']."/modelo/Usuario.class.php";
 
 class ControleUsuario
 {
+    public function verificaUser($dados)
+    {
+        $usuario = new Usuario(null, $dados['matricula'], null, null, null, null, null);
+        $cadastrado = $usuario->isCadastrado();
+        return $cadastrado;
+    }
+    
     public function verificaLogin($dados)
     {
         $usuario = new Usuario(null, $dados['matricula'], null, null, $dados['senha'], null, null);
-        $cadastrado = $usuario->isCadastrado();
-        return $cadastrado;
+        $tudoCerto = $usuario->verificaSenha();
+        return $tudoCerto;
     }
     
     public function listarUm($siapeMatricula)
