fd7eb4af101409a63a835bd69aadbd7ad7012bf7
Régis Martiny <regismartiny@gmail.com>
2017-09-18 21:02:57 -0300

    Atualizacao tabela InformacaoCurso

--- db/SqlSprint2.sql
+++ db/SqlSprint2.sql
@@ -5,6 +5,7 @@ USE `portal-ads` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `portal-ads`.`InformacaoDoCurso` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `chave` VARCHAR(100) NOT NULL,
   `titulo` VARCHAR(100) NOT NULL,
   `conteudo` VARCHAR(10000) NOT NULL,
   PRIMARY KEY (`id`))
