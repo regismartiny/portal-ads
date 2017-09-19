0dfc3d94cd0d862ec51c3f01cedfeba52069fc9a
Régis Martiny <regismartiny@gmail.com>
2017-09-18 21:56:45 -0300

    Correção sqlSprint2

--- db/SqlSprint2.sql
+++ db/SqlSprint2.sql
@@ -93,9 +93,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `portal-ads`.`InformacaoDoCurso` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `chave` VARCHAR(100) NOT NULL,
   `titulo` VARCHAR(100) NOT NULL,
   `conteudo` VARCHAR(10000) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `chave_UNIQUE` (`chave` ASC))
 ENGINE = InnoDB;
 
 
