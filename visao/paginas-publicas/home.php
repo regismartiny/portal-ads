fd1cb22fb8639dc47687d938a4fb43c50708d3ab
Regis Martiny <regis.martiny@cwi.com.br>
2017-09-28 09:27:51 -0300

    Home: página

---
+++ visao/paginas-publicas/home.php
@@ -0,0 +1,69 @@
+<div class="row">
+    <div class="col-12 mx-auto">
+        <div class="row row-grid">
+            <div class="col">
+                <div class="lista">
+                    <h2>Notícias</h2>
+                    <div class="card-deck">
+                        <div class="card">
+                            <img class="card-img-top" src="" alt="Card image cap">
+                            <div class="card-block">
+                                <h4 class="card-title">Notícia 1</h4>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                                    This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Data de publicação: 01/10/2017</small>
+                                <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=1" title="Ver mais">+</a></span>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-block">
+                                <h4 class="card-title">Notícia 2</h4>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                                    This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Data de publicação: 01/10/2017</small>
+                                <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=2" title="Ver mais">+</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-grid">
+            <div class="col">
+                <div class="lista">
+                    <h2>Eventos</h2>
+                    <div class="card-deck">
+                    <div class="card">
+                        <img class="card-img-top" src="" alt="Card image cap">
+                        <div class="card-block">
+                            <h4 class="card-title">Evento 1</h4>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                                This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Data de publicação: 01/10/2017</small>
+                            <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=1" title="Ver mais">+</a></span>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-block">
+                            <h4 class="card-title">Evento 2</h4>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                                This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Data de publicação: 01/10/2017</small>
+                            <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=2" title="Ver mais">+</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
