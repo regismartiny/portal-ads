301ab30cc21cace0a972ea44955e56db0563e6be
babalubas090 <rick@interword.com.br>
2017-09-29 01:14:31 -0600

    Updates no lstUsuario.php

--- visao/coordenador/lstUsuario.php
+++ visao/coordenador/lstUsuario.php
@@ -5,16 +5,70 @@
 ?>
 <html lang='pt-br'>
 	<head>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 		<meta charset='utf-8'>
 		<title>Listagem de Usuarios</title>
 	</head>
 	<style>
 		#container {
 			margin-top: 100px;
+		}
+		/* The switch - the box around the slider */
+		.switch {
+		position: relative;
+		display: inline-block;
+		width: 60px;
+		height: 34px;
+		}
+
+		/* Hide default HTML checkbox */
+		.switch input {display:none;}
+
+		/* The slider */
+		.slider {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: #ccc;
+		-webkit-transition: .4s;
+		transition: .4s;
+		}
+
+		.slider:before {
+		position: absolute;
+		content: "";
+		height: 26px;
+		width: 26px;
+		left: 4px;
+		bottom: 4px;
+		background-color: white;
+		-webkit-transition: .4s;
+		transition: .4s;
+		}
+
+		input:checked + .slider {
+		background-color: #2196F3;
+		}
+
+		input:focus + .slider {
+		box-shadow: 0 0 1px #2196F3;
+		}
+
+		input:checked + .slider:before {
+		-webkit-transform: translateX(26px);
+		-ms-transform: translateX(26px);
+		transform: translateX(26px);
+		}
+
+		/* Rounded sliders */
+		.slider.round {
+		border-radius: 34px;
+		}
+
+		.slider.round:before {
+		border-radius: 50%;
 		}
 	</style>
 	<body>
@@ -34,9 +88,10 @@
 							echo "<tr><th scope='row'>".$usuario->getSiapeMatricula()."</th>";
 							echo "<td>".$usuario->getNome()."</td>";
 							if($usuario->getStatus()==1){
-								echo "<td>".$usuario->getStatus()."</td></tr>";
+								//echo "<td>".$usuario->getStatus()."</td></tr>";
+								echo "<td><label class='switch'><input type='checkbox' checked><span class='slider round'></span></label><td>";
 							}else{
-								echo "<td>".$usuario->getStatus()."</td></tr>";
+								echo "<td><label class='switch'><input type='checkbox'><span class='slider round'></span></label><td>";
 							}
 
 						}
