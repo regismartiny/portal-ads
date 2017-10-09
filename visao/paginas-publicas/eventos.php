e4fd88b695009604c6e4e6e35a9d75a6d626706d
Régis Martiny <regismartiny@gmail.com>
2017-10-09 18:56:51 -0300

    Eventos

--- visao/paginas-publicas/eventos.php
+++ visao/paginas-publicas/eventos.php
@@ -4,28 +4,26 @@
         <div class="row row-grid">
             <div class="col">
                 <div class="card">
-                    <img class="card-img-top" src="" alt="Card image cap">
+                    <img class="card-img-top" src="https://even3.azureedge.net/logos/logoseminario.bb84f621ea7447b48ba0.jpg" alt="Card image cap">
                     <div class="card-block">
-                        <h4 class="card-title">Evento 1</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                            This content is a little bit longer.</p>
+                        <h4 class="card-title">I Seminário de Humanidades na Educação - GRE Metropolitana Sul</h4>
+                        <p class="card-text">É uma proposta de diálogo interdisciplinar na Área das Ciências Humanas e suas Tecnologias. Os diversos componentes curriculares (História, Geografia, Filosofia, Sociologia).</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <small class="text-muted">Data de publicação: 05/10/2017</small>
                         <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=1" title="Ver mais">+</a></span>
                     </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card">
-                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <img class="card-img-top" src="https://even3.azureedge.net/logos/Logo_evento_3.c49d67def27b47d49a42.jpg" alt="Card image cap">
                     <div class="card-block">
-                        <h4 class="card-title">Evento 2</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                            This content is a little bit longer.</p>
+                        <h4 class="card-title">IV SEMINÁRIO INSTITUCIONAL DO PIBID. III SEMANA ACADÊMICA DA LICENCIATURA EM QUÍMICA. II SEMANA ACADÊMICA DA LICENCIATURA EM PEDAGOGIA</h4>
+                        <p class="card-text">-IV SEMINÁRIO INSTITUCIONAL DO PIBID -III SEMANA ACADÊMICA DA LICENCIATURA EM QUÍMICA	-II SEMANA ACADÊMICA DA LICENCIATURA EM PEDAGOGIA.</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <small class="text-muted">Data de publicação: 05/10/2017</small>
                         <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=2" title="Ver mais">+</a></span>
                     </div>
                 </div>
