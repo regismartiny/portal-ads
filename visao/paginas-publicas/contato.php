5534a8c57011a76dba4d356fb4c716127b4acf25
Regis Martiny <regis.martiny@cwi.com.br>
2017-10-09 10:51:25 -0300

    Ajuste formulário de contato

--- visao/paginas-publicas/contato.php
+++ visao/paginas-publicas/contato.php
@@ -1,10 +1,9 @@
-<div class="row">
-    <div class="col-12 mx-auto">
+
         <h2 class="titulo">Contato</h2>
         <form method="post" action="contato.php">
             <div class="form-group row">
-                <label for="categoria" class="col-sm-12 col-md-6 col-form-label">Tipo de mensagem:</label>
-                <div class="col-sm-12 col-md-6">
+                <label for="categoria" class="col-sm-12 col-md-4 col-form-label">Tipo de mensagem:</label>
+                <div class="col-sm-12 col-md-8">
                 <select class="form-control" id="categoria" name="categoria" required>
                     <option value="cat1">Categoria 1</option>
                     <option value="cat2">Categoria 2</option>
@@ -27,7 +26,7 @@
             <div class="form-group row">
                 <label for="mensagem" class="col-sm-12 col-md-4 col-form-label">Digite sua mensagem:</label>
                 <div class="col-sm-12 col-md-8">
-                    <textarea class="form-control" rows="3" id="mensagem" name="mensagem" required></textarea>
+                    <textarea class="form-control" rows="3" id="mensagem" name="mensagem" cols="50" required></textarea>
                 </div>
             </div>
             <div class="form-group row">
@@ -36,5 +35,4 @@
                 </div>
             </div>
         </form>
-    </div>
-</div>
+ 
