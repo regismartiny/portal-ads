69c51ac98f9d3b6026b5c7634e72d1715eeac3be
Régis Martiny <regismartiny@gmail.com>
2017-10-09 18:54:23 -0300

    Home - Noticias e Eventos: Adição de conteúdo estático

--- visao/paginas-publicas/evento-detalhe.php
+++ visao/paginas-publicas/evento-detalhe.php
@@ -2,39 +2,15 @@
     <div class="col-sm-12 col-md-10 col-lg-8 mx-auto">
         <div class="card">
             <div class="card-block">
-                <h4 class="evento-detalhe card-title">Título do Evento</h4>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer.
+                <h4 class="evento-detalhe card-title">I Seminário de Humanidades na Educação - GRE Metropolitana Sul</h4>
+                <p class="card-text">É uma proposta de diálogo interdisciplinar na Área das Ciências Humanas e suas Tecnologias. Os diversos componentes curriculares (História, Geografia, Filosofia, Sociologia e Direitos Humanos) pretendem explicar a relação do homem com o meio: as humanidades abordam diversos aspectos do ser humano como individuo, pessoa e sujeito, relacionando-se consigo mesmo, o outro e o mundo. Pensando nisso a GRE Metropolitana Sul consolidou este seminário com o objetivo de socializar e propagar experiências nessa área, incentivando a participação dos professores que atuam nas diversas instituições públicas e privadas. Esse evento está inserido no Projeto METAS e faz parte das ações de formação continuada para os nossos professores.
                 </p>
             </div>
-            <img class="evento-detalhe card-img-bottom" src="./img/evento.png" alt="Imagem do evento">
+            <img class="evento-detalhe card-img-bottom" src="https://even3.azureedge.net/logos/Logo_evento_3.c49d67def27b47d49a42.jpg" alt="Imagem do evento">
             <div class="card-footer">
-                <small class="text-muted">Período: 01/09/2017 a 02/09/2017</small>
-                <div><small class="text-muted">Cidade: Feliz</small></div>
-                <div><small class="text-muted">Custo: R$ 100,00</small></div>
+                <small class="text-muted">Período: 10/10/2017 – 11/10/2017</small>
+                <div><small class="text-muted">Instituto Federal Goiano - Campus Morrinhos - Morrinhos - Goiás</small></div>
+                <div><small class="text-muted">Custo: R$ 5,00</small></div>
             </div>
         </div>
     </div>
--- visao/paginas-publicas/home.php
+++ visao/paginas-publicas/home.php
@@ -1,67 +1,60 @@
 <div class="row">
     <div class="col-12 mx-auto">
+        <h2 class="titulo">Notícias</h2>
         <div class="row row-grid">
             <div class="col">
-                <div class="lista">
-                    <h2>Notícias</h2>
-                    <div class="card-deck">
-                        <div class="card">
-                            <img class="card-img-top" src="" alt="Card image cap">
-                            <div class="card-block">
-                                <h4 class="card-title">Notícia 1</h4>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                    This content is a little bit longer.</p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Data de publicação: 01/10/2017</small>
-                                <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=1" title="Ver mais">+</a></span>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top" src="..." alt="Card image cap">
-                            <div class="card-block">
-                                <h4 class="card-title">Notícia 2</h4>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                    This content is a little bit longer.</p>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Data de publicação: 01/10/2017</small>
-                                <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=2" title="Ver mais">+</a></span>
-                            </div>
-                        </div>
+                <div class="card">
+                    <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/621/417/29/44/300/170/images.terra.com/2017/10/09/248758-trailer-de-liga-da-justica-mostra-supe-650x488-1.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">'Liga da Justiça': Superman aparece pela 1ª vez no trailer</h4>
+                        <p class="card-text">São mais de três minutos que dão um gostinho de como vai ser o filme da DC.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=1" title="Ver mais">+</a></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2017/10/09/sitereceitafederal.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">Receita abre consulta ao 5º lote de restituição do IR 2017</h4>
+                        <p class="card-text">A Receita Federal abriu hoje (9) as consultas ao quinto lote de restituição de Imposto de Renda Pessoa Física (IRPF) de 2017. O lote contempla mais de 2,3 milhões de contribuintes, com a liberação de R$ 2,8 bilhões. O valor será creditado no próximo dia 1.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=2" title="Ver mais">+</a></span>
                     </div>
                 </div>
             </div>
         </div>
         <hr class="separador-h"/>
+        <h2 class="titulo">Eventos</h2>
         <div class="row row-grid">
             <div class="col">
-                <div class="lista">
-                    <h2>Eventos</h2>
-                    <div class="card-deck">
-                    <div class="card">
-                        <img class="card-img-top" src="" alt="Card image cap">
-                        <div class="card-block">
-                            <h4 class="card-title">Evento 1</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
-                            <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=1" title="Ver mais">+</a></span>
-                        </div>
+                <div class="card">
+                    <img class="card-img-top" src="https://even3.azureedge.net/logos/logoseminario.bb84f621ea7447b48ba0.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">I Seminário de Humanidades na Educação - GRE Metropolitana Sul</h4>
+                        <p class="card-text">É uma proposta de diálogo interdisciplinar na Área das Ciências Humanas e suas Tecnologias. Os diversos componentes curriculares (História, Geografia, Filosofia, Sociologia).</p>
                     </div>
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-block">
-                            <h4 class="card-title">Evento 2</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
-                            <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=2" title="Ver mais">+</a></span>
-                        </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 05/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=1" title="Ver mais">+</a></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="https://even3.azureedge.net/logos/Logo_evento_3.c49d67def27b47d49a42.jpg" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">IV SEMINÁRIO INSTITUCIONAL DO PIBID. III SEMANA ACADÊMICA DA LICENCIATURA EM QUÍMICA. II SEMANA ACADÊMICA DA LICENCIATURA EM PEDAGOGIA</h4>
+                        <p class="card-text">-IV SEMINÁRIO INSTITUCIONAL DO PIBID -III SEMANA ACADÊMICA DA LICENCIATURA EM QUÍMICA	-II SEMANA ACADÊMICA DA LICENCIATURA EM PEDAGOGIA.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 05/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/evento-detalhe.php?id=2" title="Ver mais">+</a></span>
                     </div>
                 </div>
             </div>
