a5b761bf8af1558a93df46e6d566d4550415950f
Regis Martiny <regis.martiny@cwi.com.br>
2017-10-09 12:48:26 -0300

    Ajuste estrutura de páginas públicas de listagem

--- visao/paginas-publicas/portfolioDeProjetos.php
+++ visao/paginas-publicas/portfolioDeProjetos.php
@@ -1,62 +1,62 @@
 <div class="row">
     <div class="col-12 mx-auto">
+        <h2 class="titulo">Portfólio de Projetos</h2>
         <div class="row row-grid">
             <div class="col">
-                <h2 class="titulo">Portfólio de Projetos</h2>
-                <div class="card-deck">
-                    <div class="card">
-                        <img class="card-img-top" src="" alt="Card image cap">
-                        <div class="card-block">
-                            <h4 class="card-title">Projeto 1</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
-                            <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=1" title="Ver mais">+</a></span>
-                        </div>
+                <div class="card">
+                    <img class="card-img-top" src="" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">Projeto 1</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                            This content is a little bit longer.</p>
                     </div>
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-block">
-                            <h4 class="card-title">Projeto 2</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
-                            <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=2" title="Ver mais">+</a></span>
-                        </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=1" title="Ver mais">+</a></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">Projeto 2</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                            This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=2" title="Ver mais">+</a></span>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row row-grid">
             <div class="col">
-                <div class="card-deck">
-                    <div class="card">
-                        <img class="card-img-top" src="" alt="Card image cap">
-                        <div class="card-block">
-                            <h4 class="card-title">Projeto 3</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
-                            <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=3" title="Ver mais">+</a></span>
-                        </div>
+                <div class="card">
+                    <img class="card-img-top" src="" alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">Projeto 3</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                            This content is a little bit longer.</p>
                     </div>
-                    <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-block">
-                            <h4 class="card-title">Projeto 4</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
-                            <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=4" title="Ver mais">+</a></span>
-                        </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=3" title="Ver mais">+</a></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-block">
+                        <h4 class="card-title">Projeto 4</h4>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                            This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Data de publicação: 01/10/2017</small>
+                        <span class="see-more"><a href="/visao/paginas-publicas/projeto-detalhe.php?id=4" title="Ver mais">+</a></span>
                     </div>
                 </div>
             </div>
