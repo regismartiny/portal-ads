2a2b4e4ce5b7a9a431c229fdda948d65c32d9761
Regis Martiny <regis.martiny@cwi.com.br>
2017-10-09 11:20:05 -0300

    Adição de conteúdo estático noticias

--- visao/paginas-publicas/noticia-detalhe.php
+++ visao/paginas-publicas/noticia-detalhe.php
@@ -2,38 +2,17 @@
     <div class="col-sm-12 col-md-10 col-lg-8 mx-auto">
         <div class="card">
             <div class="card-block">
-                <h4 class="noticia-detalhe card-title">Título da Notícia</h4>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer. This is a wider card with
-                    supporting text below as a natural lead-in to additional content. This content is a little bit
-                    longer. This is a wider card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer. This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer.
+                <h4 class="noticia-detalhe card-title">'Liga da Justiça': Superman aparece pela 1ª vez no trailer</h4>
+                <p class="card-text"><p>Finalmente! Conforme prometido, o trailer final de " Liga da Justiça " foi liberado no último domingo (08) e o assunto ainda não saiu da cabeça dos fãs. E não é por menos! São mais de três minutos de muita ação, cenas inéditas e os heróis favoritos dos admiradores da DC num só vídeo. Ainda não viu? Então, aperta o play aí!.</p>
+                    <p>No trailer, podemos - finalmente - ver uma cena do Superman (Henry Cavill), que ainda não havia aparecido em nenhum material de divulgação do longa. Sua ausência é justificada pelos eventos de "Batman V Superman: A Origem da Justiça", que são cruciais para a história de "Liga da Justiça". Ou seja, se você ainda não assistiu "BvS", ainda dá tempo de ficar por dentro de tudo, viu?</p>
+                    <p>Em "Liga da Justiça", Batman (Ben Affleck) retoma sua fé na humanidade após o recente ato altruísta do Homem de Aço. É a partir disso que Bruce Wayne recruta uma legião de heróis - Mulher-Maravilha (Gal Gadot), Aquaman (Jason Momoa), Cyborg (Ray Fisher) e The Flash (Ezra Miller) - para proteger a Terra, que está sob ameaça, de um fim catastrófico.</p>
+                    <p>Vale lembrar que "Liga da Justiça" estreia no Brasil em 16 de novembro.
                 </p>
             </div>
-            <img class="noticia-detalhe card-img-bottom" src="./img/noticia.png" alt="Imagem da notícia">
+            <img class="noticia-detalhe card-img-bottom" src="https://p2.trrsf.com/image/fget/cf/621/417/29/44/300/170/images.terra.com/2017/10/09/248758-trailer-de-liga-da-justica-mostra-supe-650x488-1.jpg" alt="Imagem da notícia">
             <div class="card-footer">
-                <small class="text-muted">Fonte: Google</small>
-                <div><small class="text-muted">Categoria: Tecnologia</small></div>
+                <small class="text-muted">Fonte: Terra</small>
+                <div><small class="text-muted">Categoria: Cinema</small></div>
                 <div><small class="text-muted">Data de publicação: 01/10/2017</small></div>
             </div>
         </div>
--- visao/paginas-publicas/noticias.php
+++ visao/paginas-publicas/noticias.php
@@ -5,11 +5,10 @@
             <div class="col">
                 <div class="card-deck">
                     <div class="card">
-                        <img class="card-img-top" src="" alt="Card image cap">
+                        <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/621/417/29/44/300/170/images.terra.com/2017/10/09/248758-trailer-de-liga-da-justica-mostra-supe-650x488-1.jpg" alt="Card image cap">
                         <div class="card-block">
-                            <h4 class="card-title">Notícia 1</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
+                            <h4 class="card-title">'Liga da Justiça': Superman aparece pela 1ª vez no trailer</h4>
+                            <p class="card-text">São mais de três minutos que dão um gostinho de como vai ser o filme da DC.</p>
                         </div>
                         <div class="card-footer">
                             <small class="text-muted">Data de publicação: 01/10/2017</small>
@@ -17,11 +16,10 @@
                         </div>
                     </div>
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2017/10/09/sitereceitafederal.jpg" alt="Card image cap">
                         <div class="card-block">
-                            <h4 class="card-title">Notícia 2</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
+                            <h4 class="card-title">Receita abre consulta ao 5º lote de restituição do IR 2017</h4>
+                            <p class="card-text">A Receita Federal abriu hoje (9) as consultas ao quinto lote de restituição de Imposto de Renda Pessoa Física (IRPF) de 2017. O lote contempla mais de 2,3 milhões de contribuintes, com a liberação de R$ 2,8 bilhões. O valor será creditado no próximo dia 1.</p>
                         </div>
                         <div class="card-footer">
                             <small class="text-muted">Data de publicação: 01/10/2017</small>
@@ -35,26 +33,24 @@
             <div class="col">
                 <div class="card-deck">
                     <div class="card">
-                        <img class="card-img-top" src="" alt="Card image cap">
+                        <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2017/10/09/2017-10-09T130438Z_1_LYNXMPED980WE_RTROPTP_3_AUTOS-ANFAVEA-2018.JPG" alt="Card image cap">
                         <div class="card-block">
-                            <h4 class="card-title">Notícia 3</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
+                            <h4 class="card-title">Anfavea vê crescimento nas vendas de veículos novos em 2018</h4>
+                            <p class="card-text">As vendas de veículos no Brasil devem registrar crescimento de dois dígitos em 2018, após uma expansão de 7,3 por cento esperada para este ano, apesar das incertezas que podem ser produzidas pelo cenário eleitoral, afirmou nesta segunda-feira o presidente Associação Nacional dos Fabricantes de Veículos Automotores (Anfavea) ,Antonio Megale.</p>
                         </div>
                         <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
+                            <small class="text-muted">Data de publicação: 02/10/2017</small>
                             <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=3" title="Ver mais">+</a></span>
                         </div>
                     </div>
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2017/10/07/98048957gettyimages-692429144.jpg" alt="Card image cap">
                         <div class="card-block">
-                            <h4 class="card-title">Notícia 4</h4>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                                This content is a little bit longer.</p>
+                            <h4 class="card-title">Como uma confusão geográfica distorceu os dados do comércio entre Brasil e Coreia do Norte</h4>
+                            <p class="card-text">Para especialistas, estatísticas que indicam que compramos produtos de alta tecnologia do país de Kim Jong-un são resultado de erros nos registros de importações da Coreia do Sul.</p>
                         </div>
                         <div class="card-footer">
-                            <small class="text-muted">Data de publicação: 01/10/2017</small>
+                            <small class="text-muted">Data de publicação: 02/10/2017</small>
                             <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=4" title="Ver mais">+</a></span>
                         </div>
                     </div>
