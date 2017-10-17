41a471573328b17d4b73cdf1b828a2ad455a9db4
Régis Martiny <regismartiny@gmail.com>
2017-10-16 23:16:57 -0200

    feat(Noticias): Exibir resumo

--- controle/Util.php
+++ controle/Util.php
@@ -10,4 +10,17 @@ function clearArray($array) {
   return $array;
 }
 
+function resumir($texto, $quantCaracteres) {
+  $resumo = $texto;
+  if (strlen($resumo) > $quantCaracteres) {
+    $resumo = preg_replace('/\s+?(\S+)?$/', '', mb_substr($texto, 0, $quantCaracteres-1));
+  }
+  //adiciona 3 pontinhos se último caractere não for ponto
+  $ultimoCaractere = substr($resumo, strlen($resumo)-1, strlen($resumo));
+  if ($ultimoCaractere !== '.') {
+      $resumo = $resumo . '...';
+  }
+  return $resumo;
+}
+
 ?>
