7115c1b134e456642cebf13a2068835694cbb443
Régis Martiny <regismartiny@gmail.com>
2017-10-18 19:13:54 -0200

    feat(Noticias): Integrando listagem de noticias na home

--- visao/paginas-publicas/home.php
+++ visao/paginas-publicas/home.php
@@ -2,32 +2,39 @@
     <div class="col-12 mx-auto">
         <h2 class="titulo">Notícias</h2>
         <div class="row row-grid">
-            <div class="col">
-                <div class="card">
-                    <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/621/417/29/44/300/170/images.terra.com/2017/10/09/248758-trailer-de-liga-da-justica-mostra-supe-650x488-1.jpg" alt="Card image cap">
-                    <div class="card-block">
-                        <h4 class="card-title">'Liga da Justiça': Superman aparece pela 1ª vez no trailer</h4>
-                        <p class="card-text">São mais de três minutos que dão um gostinho de como vai ser o filme da DC.</p>
+            <?php 
+                include_once $_SERVER['DOCUMENT_ROOT']."/controle/ControleNoticia.class.php";
+                include_once $_SERVER['DOCUMENT_ROOT']."/controle/Util.php";
+
+                $cNoticia = new ControleNoticia();
+                $pagina = 1;
+                //
+                //falta implementar paginador no front
+                //
+                $noticias = $cNoticia->listarPaginado($pagina, 10);
+                
+                if ($noticias == false) {
+                    echo "Nenhuma notícia encontrada.";
+                    return;
+                }
+                
+                foreach($noticias as $noticia) {
+                    $resumo = resumir($noticia->conteudo, 230);
+            ?>
+                    <div class="col-sm-12 <?php echo (count($noticias) > 1 ? 'col-md-6' : 'col-md-8 mx-auto') ?>">
+                        <div class="card">
+                            <img class="card-img-top" src="<?php echo $noticia->imagem ?>" alt="Imagem">
+                            <div class="card-block">
+                                <h4 class="card-title"><?php echo $noticia->titulo ?></h4>
+                                <p class="card-text"><?php echo $resumo ?></p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Data de publicação: <?php echo date("d/m/Y", strtotime($noticia->dataPublicacao)) ?></small>
+                                <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=<?php echo $noticia->id ?>" title="Ver mais">+</a></span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Data de publicação: 01/10/2017</small>
-                        <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=1" title="Ver mais">+</a></span>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <img class="card-img-top" src="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2017/10/09/sitereceitafederal.jpg" alt="Card image cap">
-                    <div class="card-block">
-                        <h4 class="card-title">Receita abre consulta ao 5º lote de restituição do IR 2017</h4>
-                        <p class="card-text">A Receita Federal abriu hoje (9) as consultas ao quinto lote de restituição de Imposto de Renda Pessoa Física (IRPF) de 2017. O lote contempla mais de 2,3 milhões de contribuintes, com a liberação de R$ 2,8 bilhões. O valor será creditado no próximo dia 1.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Data de publicação: 01/10/2017</small>
-                        <span class="see-more"><a href="/visao/paginas-publicas/noticia-detalhe.php?id=2" title="Ver mais">+</a></span>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>
         </div>
         <hr class="separador-h"/>
         <h2 class="titulo">Eventos</h2>
