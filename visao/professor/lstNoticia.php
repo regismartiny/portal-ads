d5fa08ae3989e99ecb46842f3f8042ac1cd2e5ec
Régis Martiny <regismartiny@gmail.com>
2017-10-27 14:18:37 -0200

    refactor(noticia): refatorando noticia e adicionando modificacao de status

--- visao/professor/lstNoticia.php
+++ visao/professor/lstNoticia.php
@@ -1,46 +1,60 @@
 <?php
 	session_start();
 	if (!isset($_SESSION["tipoUsuario"]) || $_SESSION["tipoUsuario"]!=2 || !isset($_COOKIE["702741445"])){
-		header( 'Location: /controle/logout.php' );
+		header('Location: /controle/logout.php');
+		return;
 	}
-	else{
-		include_once $_SERVER['DOCUMENT_ROOT']."/controle/ControleNoticia.class.php";
-		include_once $_SERVER["DOCUMENT_ROOT"]."/modelo/CategoriaNoticia.class.php";
-		$nControle = new ControleNoticia();
-		$categoriaNoticia = new CategoriaNoticia();
 
-		$noticias = $nControle->minhasNoticias($_SESSION['matricula']);
-	}
+	include_once $_SERVER['DOCUMENT_ROOT']."/controle/ControleNoticia.class.php";
+	include_once $_SERVER['DOCUMENT_ROOT']."/controle/Util.php";
 	
+	$nControle = new ControleNoticia();
+
+	if (!empty($_POST['alterarStatusId'])) {
+		$dados = clearArray($_POST);
+		$nControle->modificarStatusNoticia($dados['alterarStatusId']);
+	}
+
+	$noticias = $nControle->minhasNoticias($_SESSION['matricula']);
 ?>
-<div class="row">
 	<div class="col mx-auto">
 		<h2 class="titulo">Minhas Notícias</h2>
-		<?php
-			if($noticias!=false){
-				echo "<table class='table table-hover'>
-						<thead>
-							<tr>
-							<th>Número</th>
-							<th>Titulo</th>
-							<th>Data da Publicação</th>
-							<th>Status</th>
-							</tr>
-						</thead>";
-				foreach($noticias as $noticia){
-					//print_r($noticia);	
-					echo "<tr><th scope='row'>".$noticia->id."</th>";
-					echo "<td><a href='/visao/professor/editaNoticia.php?idNoticia=".$noticia->id."'>".$noticia->titulo."</td>";
-					echo "<td>".$noticia->dataCadastro."</td>";
-					if($noticia->status == 1){
-						echo "<td><label class='switch'><input type='checkbox' checked><span class='slider round'></span></label><td>";
-					}else{
-						echo "<td><label class='switch'><input type='checkbox' ><span class='slider round'></span></label><td>";
-					}
-
-				}
-				echo "</table>";
-			}
-		?>
+		<?php if($noticias != false) { ?>
+			<table class='table table-hover'>
+				<thead>
+					<tr>
+						<th>Número</th>
+						<th>Titulo</th>
+						<th>Data da Publicação</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+			<?php foreach($noticias as $noticia) {
+				//print_r($noticia);	
+				$ativa = $noticia->status == 1 
+			?>
+				<tr>
+					<th scope='row'><?php echo $noticia->id ?></th>
+					<td><a href='/visao/professor/editaNoticia.php?idNoticia="<?php echo $noticia->id ?>"'><?php echo $noticia->titulo ?></td>
+					<td><?php echo $noticia->dataCadastro ?></td>					
+					<td>
+						<label class='switch'>
+							<input type='checkbox' onclick="modificarStatus(<?php echo $noticia->id ?>)"<?php if($ativa) echo ' checked' ?>>
+							<span class='slider round'></span>
+						</label>
+					</td>
+				</tr>
+			<? } ?>
+			</table>
+		<?php } ?>
 	</div>
 </div>
+<script>
+	function modificarStatus(id) {
+		$.ajax({
+			type: 'POST',
+			url: '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>',
+			data: 'alterarStatusId=' + id
+		});
+	}
+</script>
